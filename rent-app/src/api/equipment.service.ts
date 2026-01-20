ee14c3c3673df5ce979ea73288f9d4713231d7ea
Andre <andrehenriq2005@gmail.com>
2026-01-20 13:59:08 -0400

    chore(equipments): add real getEquipments and comment mocked implementation

--- rent-app/src/api/equipment.service.ts
+++ rent-app/src/api/equipment.service.ts
@@ -1,8 +1,14 @@
-import { equipmentsMock } from '../mocks/equipment.mock';
+// import { equipmentsMock } from '../mocks/equipment.mock';
+
+// export async function getEquipments() {
+//   // simula delay da API
+//   await new Promise((resolve) => setTimeout(resolve, 500));
+
+//   return equipmentsMock;
+// }
+import { api } from './api';
 
 export async function getEquipments() {
-  // simula delay da API
-  await new Promise((resolve) => setTimeout(resolve, 500));
-
-  return equipmentsMock;
+  const response = await api.get('/equipments');
+  return response.data;
 }
